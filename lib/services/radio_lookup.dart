0062c3f023e65600411c36e19d0dd87266eb16bd
Colin Constable <colin@constable.cc>
2021-11-11 10:53:28 -0800

    a

--- lib/services/radio_lookup.dart
+++ lib/services/radio_lookup.dart
@@ -5,15 +5,14 @@ import 'dart:convert';
 void lookupRadio(String atsign, String radio, PublicHamRadio hamRadio) async {
   print('waiting');
   http.Response result = await http.get(
-      Uri.parse("http://localhost:8080/api?atp=public.ic7100.ai6bh@ai6bh"));
+      Uri.parse('http://localhost:8080/api?atp=public.ic7100.ai6bh@ai6bh'));
   // headers: {"Accept": "application/json",
-  //           "Access-Control-Allow-Origin": "*",
-  //           });
-  // Uri.parse('https://wavi.ng/api?atp=public.$hamradio.ai6bh@$atsign&html'));
+  //    headers: {       "Access-Control-Allow-Origin": "*",
+  //         });
+  // Uri.parse('https://wavi.ng/api?atp=public.$radio.ai6bh@$atsign&html'));
   print(result.body);
 
   var radioMap = jsonDecode(result.body);
   var radioJson = jsonDecode(radioMap[0]['public.$radio.$atsign']);
- hamRadio = PublicHamRadio.fromJson(radioJson);
-
+  hamRadio = PublicHamRadio.fromJson(radioJson);
 }
